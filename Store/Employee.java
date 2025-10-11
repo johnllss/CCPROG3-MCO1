42c57f9b0ec531b860529bc0b023eb1e80c78e4d
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-10-11 23:48:53 +0800

    Create Employee.java

---
+++ Store/Employee.java
@@ -0,0 +1,29 @@
+public class Employee extends User {
+    private String role;
+
+    public Employee()
+    {
+        super();
+        this.role = "N/A";
+    }
+
+    public Employee(String uniqueID, String name, String email, String password, String role) {
+        super(uniqueID, name, email, password);
+        this.role = role;
+    }
+
+    public String getRole()
+    {
+        return this.role;
+    }
+
+    public void setRole(String newRole)
+    {
+        this.role = newRole;
+    }
+
+    public String transformToString()
+    {
+        return String.format("Name = %s, Role = %s]", getName(), role);
+    }
+}
