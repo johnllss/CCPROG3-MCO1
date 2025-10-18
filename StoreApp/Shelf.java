6624688af58f4a9c76ef99d6e7a8d2b2a2efcd1a
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-10-18 22:10:28 +0800

    Update Shelf.java

--- StoreApp/Shelf.java
+++ StoreApp/Shelf.java
@@ -29,7 +29,7 @@ public class Shelf {
             return false;
         } else 
         {
-            products.add(p);
+            products.add(product);
             return true;
         }
     }
