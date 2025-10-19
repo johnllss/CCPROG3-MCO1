96723da2a51e11bdc0c34eea4df4f71c40f3860e
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-10-19 22:22:23 +0800

    Update StoreDriver.java
    Added method declarations

--- StoreApp/StoreDriver.java
+++ StoreApp/StoreDriver.java
@@ -1,5 +1,50 @@
 package StoreApp;
 
 public class StoreDriver {
-    
+    private Inventory inventory;
+    private Customer customer;
+
+    public static void main(String args[]) {
+
+    }
+
+    public void openConvenienceStore()
+    {
+
+    }
+
+    public void handleCustomer()
+    {
+
+    }
+
+    public void handleInventoryManagement()
+    {
+
+    }
+
+    private void addProduct()
+    {
+
+    }
+
+    private void restockProduct()
+    {
+
+    }
+
+    private void removeProduct()
+    {
+
+    }
+
+    private void updateProduct()
+    {
+
+    }
+
+    private void displayExpiredProducts()
+    {
+        
+    }
 }
