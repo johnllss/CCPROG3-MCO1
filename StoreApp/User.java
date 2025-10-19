f0a57bc3aee567a02b73276804f44506926f4b0c
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-10-19 19:05:16 +0800

    Updated User's ID to int

--- StoreApp/User.java
+++ StoreApp/User.java
@@ -4,21 +4,22 @@ package StoreApp;
  */
 public class User {
     private int uniqueID;
+    private static int uniqueIDCounter = 0;
     private String name;
     private String email;
     private String password;
 
     public User() 
     {
-        this.uniqueID = "N/A";
+        this.uniqueID = ++uniqueIDCounter; // pre-increment so first instance is int 1
         this.name = "N/A";
         this.email = "N/A";
         this.password = "N/A";
     }
 
-    public User(String uniqueID, String name, String email, String password)
+    public User(String name, String email, String password)
     {
-        this.uniqueID = uniqueID;
+        this.uniqueID = ++uniqueIDCounter;
         this.name = name;
         this.email = email;
         this.password = password;
