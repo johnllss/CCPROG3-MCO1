aa235ab90d9d7502a9528f939b4c663fc24fc1e0
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-10-22 23:37:52 +0800

    Item's Javadoc

--- StoreApp/Item.java
+++ StoreApp/Item.java
@@ -4,10 +4,10 @@ public class Item {
     private Product product;
     private int quantity;
 
-    /***
-     *
-     * @param product
-     * @param quantity
+    /**
+     * Class Item parameterized constructor
+     * @param product is the product being referenced
+     * @param quantity is the quantity placed by the Customer
      */
     public Item(Product product, int quantity)
     {
@@ -15,36 +15,36 @@ public class Item {
         this.quantity = quantity;
     }
 
-    /***
-     *
-     * @return
+    /**
+     * This method calculates for the Item's subtotal price in the Cart. 
+     * @return double for the subtotal price of the Item in the Cart.
      */
     public double calculateItemSubtotal()
     {
         return this.product.getProductPrice() * this.quantity;
     }
 
-    /***
-     *
-     * @return
+    /**
+     * This is a getter method to get the referenced product.
+     * @return Product is the product and its details.
      */
     public Product getProduct()
     {
-        return product;
+        return this.product;
     }
 
-    /***
-     *
-     * @return
+    /**
+     * This is a getter method to get the quantity of the Item placed by the Customer.
+     * @return int is the quantity in this class.
      */
     public int getQuantity()
     {
-        return quantity;
+        return this.quantity;
     }
 
-    /***
-     *
-     * @param quantity
+    /**
+     * This is a setter method to set the new quantity as desired by the Customer.
+     * @param quantity is the new quantity.
      */
     public void setQuantity(int quantity)
     {
