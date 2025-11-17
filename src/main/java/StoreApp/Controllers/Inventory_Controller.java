a69c58183f7856ced9a130446b30a7c275a25248
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-11-17 16:45:19 +0800

    Inventory: Controller update for logout button process

--- src/main/java/StoreApp/Controllers/Inventory_Controller.java
+++ src/main/java/StoreApp/Controllers/Inventory_Controller.java
@@ -6,12 +6,22 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class Inventory_Controller implements Initializable {
 
-    @FXML
-    private ChoiceBox<String> choiceBox;
+    @FXML private ChoiceBox<String> choiceBox;
+    @FXML private Button logout_btn;
+    @FXML private AnchorPane scenePane;
+
+    private Stage stage;
 
     private String[] categories = {"food", "beverage", "medicine", "household"};
 
@@ -23,6 +33,20 @@ public class Inventory_Controller implements Initializable {
     public void getCategory(ActionEvent event)
     {
         String choice = choiceBox.getValue();
+    }
 
+    public void logout(ActionEvent event)
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Lougout");
+        alert.setHeaderText("You are about to logout.");
+        alert.setContentText("Do you want to save before exiting?");
+
+        if (alert.showAndWait().get() == ButtonType.OK)
+        {
+            stage = (Stage)scenePane.getScene().getWindw();
+            System.out.println("You successfully logged out!");
+            stage.close;
+        }
     }
 }
