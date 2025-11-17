7098cdb6f7c19cdf43c4c20e864656c551614c59
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-11-17 16:51:02 +0800

    Implementation of employee logging out

--- src/main/java/StoreApp/Models/Main.java
+++ src/main/java/StoreApp/Models/Main.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    @Override
     public void start(Stage stage) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("MainMenu_View.fxml"));
@@ -14,12 +17,31 @@ public class Main extends Application {
             stage.setScene(scene);
             stage.setTitle("StoreApp");
             stage.show();
+
+            stage.setOnCloseRequest(event -> {
+                event.consume(); // prevents from closing the program when pressing the X button
+                logout(stage);
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public void logout(Stage stage)
+    {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about to logout!");
+        alert.setContentText("Do you want to save before exiting?: ");
+
+        if (alert.showAndWait().get() == ButtonType.OK)
+        {
+            System.out.println("You successfully logged out!");
+            stage.close();
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
-
 }
