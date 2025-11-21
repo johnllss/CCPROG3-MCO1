c1bc09836a6603bcdd0497b9fb136b1d4c116fcf
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-11-22 02:32:27 +0800

    Cart: Finalized methods locations

--- src/main/java/StoreApp/Controllers/Cart_Controller.java
+++ src/main/java/StoreApp/Controllers/Cart_Controller.java
@@ -3,6 +3,7 @@ package StoreApp.Controllers;
 import StoreApp.Models.Cart_Model;
 import StoreApp.Models.Customer_Model;
 import StoreApp.Models.Item_Model;
+import StoreApp.Models.Product_Model;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -34,13 +35,9 @@ public class Cart_Controller {
 
     }
 
-    // TODO Populating Table itself with dynamic buttons
-
     @FXML
     public void initialize()
     {
-
-        // TODO
         updateProductBoxDisplay();
         updateAllTotals();
     }
@@ -58,7 +55,7 @@ public class Cart_Controller {
 
     private void updateAllTotals()
     {
-        double subTotal = 0; // TODO: modify to calculate/get the subtotal
+        double subTotal = calculateCartSubTotal();
         double vat = subTotal * 0.10; // TODO: modify to include seniority validation
         double total = subTotal + vat;
 
@@ -79,5 +76,77 @@ public class Cart_Controller {
     {
         System.out.println("Returning");
         // TODO load back to fxml of product listings
+    }
+
+    /**
+     * Is a method which checks if the desired product has enough stock/quantity in order to fulfill the customer's request.
+     * @param product is the product that wants to be added to the cart
+     * @param quantity is the number of products desired to be added to cart
+     * @return a boolean to signify success / failure
+     */
+    public boolean addItem(Product_Model product, int quantity) {
+        if (product.getProductQuantity() < quantity) {
+            return false;
+        } else {
+            cart.getItems().add(new Item_Model(product, quantity));
+            return true;
+        }
+    }
+
+    /**
+     * Method which iterates through all items in cart and checks if product that wants to be removed is part of the cart
+     * @param productID is the ID of the product being removed
+     * @return boolean indicating success/failure of search
+     */
+    public boolean removeItem(int productID) {
+        for (Item_Model i : cart.getItems()) {
+            if (i.getProduct().getProductID() == productID) {
+                cart.getItems().remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Method which iterates through all the items in the cart to find the item being searched
+     * @param id is the id of the product that wants to be found
+     * @return Item that is searched
+     */
+    public Item_Model findItem(int id) {
+        for (Item_Model i : cart.getItems()) {
+            if (i.getProduct().getProductID() == id) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Updates the quantity of the product in cart
+     * @param id id of the product that wants to be updated
+     * @param amount quantity of the product
+     * @return boolean, shows success or failure of the process
+     */
+    public boolean updateQuantity(int id, int amount) {
+        for (Item_Model i : cart.getItems()) {
+            if (i.getProduct().getProductID() == id && i.getProduct().getProductQuantity() >= amount) {
+                i.setQuantity(amount);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Iterates and calculates the total price for every item in the cart.
+     * @return double (total price)
+     */
+    public double calculateCartSubTotal() {
+        double subTotal = 0;
+        for (Item_Model i : cart.getItems()) {
+            subTotal += i.getProduct().getProductPrice() * i.getQuantity();
+        }
+        return subTotal;
     }
 }
--- src/main/java/StoreApp/Models/Cart_Model.java
+++ src/main/java/StoreApp/Models/Cart_Model.java
@@ -15,7 +15,23 @@ public class Cart_Model {
     public ArrayList<Item_Model> getItems() {
         return items;
     }
+
     public void setItems(ArrayList<Item_Model> items) {
         this.items = items;
+    }
+
+    /**
+     * Method that checks if the cart is empty
+     * @return boolean to show if empty = true or not empty = false
+     */
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    /**
+     * Method that removes every item in the cart
+     */
+    public void clearCart() {
+        items.clear();
     }
 }
