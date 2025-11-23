5ec1199dc6231230e04ee3f220ae81bdcf3a8fcd
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-11-23 20:33:48 +0800

    Shelf: finished transferring methods

--- src/main/java/StoreApp/Models/Shelf_Model.java
+++ src/main/java/StoreApp/Models/Shelf_Model.java
@@ -15,50 +15,95 @@ public class Shelf_Model {
      * @param category is the category for the Shelf
      * @param maxCapacity is the max number of Products in the Shelf
      */
-    public Shelf_Model(String category, int maxCapacity)
-    {
+    public Shelf_Model(String category, int maxCapacity) {
         this.shelfID = ++shelfIDCounter;
         this.category = category;
         this.maxCapacity = maxCapacity;
         this.products = new ArrayList<Product_Model>();
     }
-    public int getShelfID() {
-        return shelfID;
-    }
-    public void setShelfID(int shelfID) {
-        this.shelfID = shelfID;
-    }
-    public String getShelfCategory() {
-        return category;
-    }
-    public void setShelfCategory(String category) {
-        this.category = category;
-    }
-    public int getCurrentCapacity() {
-        return currentCapacity;
-    }
-    public void setCurrentCapacity(int currentCapacity) {
-        this.currentCapacity = currentCapacity;
-    }
-    public int getMaxCapacity() {
-        return maxCapacity;
-    }
-    public void setMaxCapacity(int maxCapacity) {
-        this.maxCapacity = maxCapacity;
-    }
-    public ArrayList<Product_Model> getProductsOnShelf() {
-        return products;
-    }
-    public void setProducts(ArrayList<Product_Model> products) {
-        this.products = products;
-    }
-    public void addProduct(Product_Model product)
-    {
+
+    /**
+     * This method adds a product to the Shelf.
+     * @param product is the product to be added.
+     */
+    public void addProductToShelf(Product_Model product) {
         this.products.add(product);
     }
-    public void removeProduct(Product_Model product)
-    {
+
+    /**
+     * This method removes a product to the Shelf.
+     * @param product is the product to be added.
+     */
+    public void removeProductFromShelf(Product_Model product) {
         this.products.remove(product);
     }
 
+    /**
+     * This is a getter method to get the Shelf's shelfID.
+     * @return int for shelfID.
+     */
+    public int getShelfID() {
+        return shelfID;
+    }
+
+    /**
+     * This method sets the Shelf's ID to a given ID.
+     * @param shelfID is the new given shelfID
+     */
+    public void setShelfID(int shelfID) {
+        this.shelfID = shelfID;
+    }
+
+    /**
+     * This is a getter method to get the Shelf's category.
+     * @return int for category.
+     */
+    public String getShelfCategory() {
+        return category;
+    }
+
+    /**
+     * This method sets the Shelf's category.
+     * @param newCategory is the new category to be used.
+     *
+     */
+    public void setShelfCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * This is a getter method to get the Shelf's currentCapacity.
+     * @return int for currentCapacity.
+     */
+    public int getCurrentCapacity() {
+        return currentCapacity;
+    }
+
+    public void setCurrentCapacity(int currentCapacity) {
+        this.currentCapacity = currentCapacity;
+    }
+
+    /**
+     * This is a getter method to get the Shelf's maxCapacity.
+     * @return int for maxCapacity.
+     */
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    /**
+     * This is a getter method to get the Shelf's products.
+     * @return ArrayList Product - All Shelf's Products
+     */
+    public ArrayList<Product_Model> getProductsOnShelf() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product_Model> products) {
+        this.products = products;
+    }
 }
