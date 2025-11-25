0d04bffa57a91c25a5d656bf7a9e42bf9cfa2c2c
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-11-25 19:13:14 +0800

    finished javadocs for Employee_Model

--- src/main/java/StoreApp/Models/Employee_Model.java
+++ src/main/java/StoreApp/Models/Employee_Model.java
@@ -3,11 +3,21 @@ package StoreApp.Models;
 public class Employee_Model extends User_Model {
     private String role;
 
+    /**
+     * Class Employee_Model default constructor
+     */
     public Employee_Model() {
         super();
         this.role = "N/A";
     }
 
+    /**
+     * Class Employee_Model parameterized constructor
+     * @param name is the employee's name.
+     * @param email is the employee's email.
+     * @param password is the employee's password.
+     * @param role is the employee's role.
+     */
     public Employee_Model(String name, String email, String password, String role)
     {
         super(name, email, password);
@@ -30,10 +40,18 @@ public class Employee_Model extends User_Model {
         return false;
     }
 
+    /**
+     * This is a getter method to get Employee's role.
+     * @return String for role.
+     */
     public String getRole() {
         return role;
     }
 
+    /**
+     * This sets Employee's role.
+     * @param role is the role to be assigned.
+     */
     public void setRole(String role) {
         this.role = role;
     }
