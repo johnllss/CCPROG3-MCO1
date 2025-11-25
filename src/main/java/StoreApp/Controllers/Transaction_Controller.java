1e8c67006af79d1db38611c6faccebaad3f327a0
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-11-25 15:01:23 +0800

    initialize() done

--- src/main/java/StoreApp/Controllers/Transaction_Controller.java
+++ src/main/java/StoreApp/Controllers/Transaction_Controller.java
@@ -1,5 +1,50 @@
 package StoreApp.Controllers;
 
+import java.lang.classfile.Label;
+
+import StoreApp.Models.Cart_Model;
+import StoreApp.Models.Customer_Model;
+import StoreApp.Models.Inventory_Model;
+import StoreApp.Models.Transaction_Model;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
 public class Transaction_Controller {
-    
+    @FXML private TextField fullNameText;
+    @FXML private TextField emailText;
+    @FXML private CheckBox membershipChewBox;
+    @FXML private TextField membershipNumberText;
+    @FXML private CheckBox seniorCheckBox;
+    @FXML private TextField ageText;
+    @FXML private CheckBox cashCheckBox;
+    @FXML private CheckBox cardCheckBox;
+    @FXML private TextField amountText;
+    @FXML private TextField accountNumberText;
+    @FXML private TextField cvvText;
+    @FXML private TextField expiryDateText;
+    @FXML private Label subtotalLabel;
+    @FXML private Label taxLabel;
+    @FXML private Label discountLabel;
+    @FXML private Label totalLabel;
+    @FXML private Button processPaymentBtn;
+    @FXML private Button cancelBtn;
+
+    private Customer_Model customer;
+    private Cart_Model cart;
+    private Inventory_Model inventory;
+    private Transaction_Model transaction;
+
+    /**
+     * This method initializes the controller after FXML elements are loaded, setting up the initial state of UI elements.
+     */
+    @FXML
+    public void initialize() {
+        membershipNumberText.setDisable(true);
+        ageText.setDisable(true);
+        accountNumberText.setDisable(true);
+        cvvText.setDisable(true);
+        expiryDateText.setDisable(true);
+    }
 }
