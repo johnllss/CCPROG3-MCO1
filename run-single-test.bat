5472feba7670c2400fadcf70e2f4b361b2a0ebcf
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-11-26 17:55:50 +0800

    test updates

--- run-single-test.bat
+++ run-single-test.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 REM Batch script to run a single test class
 
 if "%1"=="" (
@@ -13,11 +14,19 @@ set BIN_MAIN=%PROJECT_ROOT%bin\main
 set BIN_TEST=%PROJECT_ROOT%bin\test
 set LIB=%PROJECT_ROOT%lib
 
+REM Build the classpath with all JAR files
+set CLASSPATH=%BIN_MAIN%;%BIN_TEST%
+for %%i in ("%LIB%\*.jar") do (
+    if not "%%i"=="%LIB%\junit-platform-console-standalone-1.10.0.jar" (
+        set CLASSPATH=!CLASSPATH!;%%i
+    )
+)
+
 echo Running test: StoreApp.Models.%1
 echo.
 
 java -jar "%LIB%\junit-platform-console-standalone-1.10.0.jar" ^
-    --class-path "%BIN_MAIN%;%BIN_TEST%;%LIB%\*" ^
+    --class-path "!CLASSPATH!" ^
     --select-class StoreApp.Models.%1
 
 pause
